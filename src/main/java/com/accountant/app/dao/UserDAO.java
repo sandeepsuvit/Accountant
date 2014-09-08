39621472d0ba31c8ad122fba86c926daca7d40f8
Sandeep K Nair <sandeepsuvit@gmail.com>
2014-09-08 15:44:59 +0800

    updated user DAO

--- src/main/java/com/accountant/app/dao/UserDAO.java
+++ src/main/java/com/accountant/app/dao/UserDAO.java
@@ -3,12 +3,13 @@ package com.accountant.app.dao;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.accountant.app.beans.Users;
-import com.accountant.app.dao.beanMapper.UserMapper;
+import com.accountant.app.services.UserService;
 
 /**
  * 
@@ -22,58 +23,85 @@ import com.accountant.app.dao.beanMapper.UserMapper;
  *
  * Mar 1, 2014 | 3:21:33 PM | 2014
  * 
- * Interface class for the user detials
+ * Interface class for the user details
  */
 @Repository("userDao")
-public class UserDAO extends AccountantGenericDAO{
+public class UserDAO extends AccountantGenericDAO implements UserService{
 	
-private static final Logger log = Logger.getLogger(UserDAO.class);
-	
+	private static final Logger log = Logger.getLogger(UserDAO.class);
+
 	/**
-	 * Inserting user details to the database
+	 * Method to create a new user detail
+	 * 
 	 * @param users
+	 * @return the boolean status weather the records has been inserted
 	 */
-	public void insertUserDetails(Users users){
-		String sql = "INSERT INTO users_tb (userFname, userLname, userEmail) VALUES ("
-									+ ":userFname,"
-									+ ":userLname,"
-									+ ":userEmail"
-									+ ")";
-		if(log.isDebugEnabled()){
-			log.debug("METHOD :: insertUserDetails :: SQL : "+sql);
+	public Boolean createNewUser(Users users) {
+		log.info("ENTERING METHOD :: createNewUser");
+		String sql = "INSERT INTO users_tb (user_acc_id, user_password, user_status) VALUES ("
+				+ ":userAcId," + ":userPassword," + ":userStatus" + ")";
+
+		if (log.isDebugEnabled()) {
+			log.debug("METHOD :: createNewUser :: SQL : " + sql);
 		}
 
-		SqlParameterSource parameterSource = new MapSqlParameterSource("userFname",users.getUserFname())
-												.addValue("userLname", users.getUserLname())
-												.addValue("userEmail", users.getUserEmail());
-		this.getNamedParameterJdbcTemplate().update(sql, parameterSource);
+		try {
+
+			this.getJdbcTemplate().update(
+					sql,
+					new Object[] { users.getUserAccId(),
+							users.getUserPassword(), users.getUserStatus() });
+
+			log.info("EXITING METHOD :: createNewUser");
+
+			return true;
+		} catch (Exception exception) {
+			log.error("METHOD :: Couldn't insert the record : " + exception);
+			log.info("EXITING METHOD :: createNewUser");
+			return false;
+		}
 	}
-	
-	
+
 	/**
-	 * Returns a user details
+	 * Method to return a user detail
+	 * 
 	 * @param userId
-	 * @return
+	 * @return the Users object
 	 */
-	public Users getUserDetailsById(int userId){
-		String sql = "SELECT * FROM users_tb WHERE userId = :userId";
-		if(log.isDebugEnabled()){
-			log.debug("METHOD :: getUserDetailsById :: SQL : "+sql);
+	public Users getUserDetailsById(int userId) {
+		log.info("ENTERING METHOD :: getUserDetailsById");
+
+		String sql = "SELECT user_acc_id, user_status FROM users_tb WHERE u_id = :uId";
+
+		if (log.isDebugEnabled()) {
+			log.debug("METHOD :: getUserDetailsById :: SQL : " + sql);
 		}
-		SqlParameterSource parameterSource = new MapSqlParameterSource("userId",userId);
-		return this.getNamedParameterJdbcTemplate().queryForObject(sql, parameterSource, new UserMapper());
+
+		SqlParameterSource parameterSource = new MapSqlParameterSource("uId",
+				userId);
+
+		log.info("EXITING METHOD :: getUserDetailsById");
+		return this.getNamedParameterJdbcTemplate().queryForObject(sql,
+				parameterSource, new BeanPropertyRowMapper<Users>(Users.class));
 	}
-	
-	
+
 	/**
-	 * Gets the list of all the users in the user table
-	 * @return
+	 * Method to return the list of all users
+	 * 
+	 * @return the list id Users
 	 */
-	public List<Users> getAllUserDetails(){
+	public List<Users> getListOfAllUser() {
+		log.info("ENTERING METHOD :: getAllUserDetails");
+
 		String sql = "SELECT * FROM users_tb";
-		if(log.isDebugEnabled()){
-			log.debug("METHOD :: getAllUserDetails :: SQL : "+sql);
+
+		if (log.isDebugEnabled()) {
+			log.debug("METHOD :: getAllUserDetails :: SQL : " + sql);
 		}
-		return this.getJdbcTemplate().query(sql, new UserMapper());
+
+		log.info("EXITING METHOD :: getAllUserDetails");
+
+		return this.getJdbcTemplate().query(sql,
+				new BeanPropertyRowMapper<Users>(Users.class));
 	}
 }
