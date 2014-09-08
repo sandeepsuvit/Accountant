96e13edb1cc3e1bef7ef4c93acb784e521294d22
Sandeep K Nair <sandeepsuvit@gmail.com>
2014-09-08 15:42:15 +0800

    Updated Users VO

--- src/main/java/com/accountant/app/beans/Users.java
+++ src/main/java/com/accountant/app/beans/Users.java
@@ -16,81 +16,62 @@ package com.accountant.app.beans;
  * Mar 1, 2014 | 3:15:19 PM | 2014
  *
  * This is a User bean class for mapping the current
- * user detials
+ * user details
  */
 public class Users {
-	private int userId;
-	private String userFname;
-	private String userLname;
-	private String userEmail;
-	
+	private int uId;
+	private String userAccId;
+	private String userPassword;
+	private String userStatus;
 	/**
-	 * Empty constructor
+	 * @return the uId
 	 */
-	public Users(){
-		
+	public int getuId() {
+		return uId;
+	}
+	/**
+	 * @param uId the uId to set
+	 */
+	public void setuId(int uId) {
+		this.uId = uId;
+	}
+	/**
+	 * @return the userAccId
+	 */
+	public String getUserAccId() {
+		return userAccId;
+	}
+	/**
+	 * @param userAccId the userAccId to set
+	 */
+	public void setUserAccId(String userAccId) {
+		this.userAccId = userAccId;
+	}
+	/**
+	 * @return the userPassword
+	 */
+	public String getUserPassword() {
+		return userPassword;
+	}
+	/**
+	 * @param userPassword the userPassword to set
+	 */
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	/**
+	 * @return the userStatus
+	 */
+	public String getUserStatus() {
+		return userStatus;
+	}
+	/**
+	 * @param userStatus the userStatus to set
+	 */
+	public void setUserStatus(String userStatus) {
+		this.userStatus = userStatus;
 	}
 	
-	/**
-	 * @param userId
-	 * @param userFname
-	 * @param userLname
-	 * @param userEmail
-	 */
-	public Users( String userFname, String userLname,
-			String userEmail) {
-		this.userFname = userFname;
-		this.userLname = userLname;
-		this.userEmail = userEmail;
-	}
-
-
-	/**
-	 * @return the userId
-	 */
-	public int getUserId() {
-		return userId;
-	}
-	/**
-	 * @param userId the userId to set
-	 */
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-	/**
-	 * @return the userFname
-	 */
-	public String getUserFname() {
-		return userFname;
-	}
-	/**
-	 * @param userFname the userFname to set
-	 */
-	public void setUserFname(String userFname) {
-		this.userFname = userFname;
-	}
-	/**
-	 * @return the userLname
-	 */
-	public String getUserLname() {
-		return userLname;
-	}
-	/**
-	 * @param userLname the userLname to set
-	 */
-	public void setUserLname(String userLname) {
-		this.userLname = userLname;
-	}
-	/**
-	 * @return the userEmail
-	 */
-	public String getUserEmail() {
-		return userEmail;
-	}
-	/**
-	 * @param userEmail the userEmail to set
-	 */
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
+	
+	
 }
